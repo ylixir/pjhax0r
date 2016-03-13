034c8127d155db89598b462d95450b0e76af91b0
Jon Allen <ylixir@gmail.com>
2016-03-13 14:04:39 -0500

    fresh autogenerated state

---
+++ desktop/src/com/pjhax0rs/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pjhax0rs.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pjhax0rs.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
